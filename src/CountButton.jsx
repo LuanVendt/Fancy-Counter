f5c1dcd5617beba26752ffee70ad02219618b8da
LuanVendt <luanvendt@gmail.com>
2025-01-18 17:37:20 -0300

    refactor: improving countButton logic

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,16 +1,13 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export default function CountButton({ type, setCount }) {
-  const handlePlusButtonClick = () => setCount((prev) => prev + 1);
-  const handleMinusButtonClick = () => setCount((prev) => prev - 1);
+  const handleClick = () =>
+    type === "plus"
+      ? setCount((prev) => prev + 1)
+      : setCount((prev) => prev - 1);
 
   return (
-    <button
-      onClick={
-        type === "minus" ? handleMinusButtonClick : handlePlusButtonClick
-      }
-      className="count-btn"
-    >
+    <button onClick={handleClick} className="count-btn">
       {type === "minus" ? (
         <MinusIcon className="count-btn-icon" />
       ) : (
