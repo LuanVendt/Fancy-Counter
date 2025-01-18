bcf133143c1fb3d12b9521c148381dbcb76fb61b
LuanVendt <luanvendt@gmail.com>
2025-01-18 17:45:22 -0300

    feat: add a bottom limit to the counter

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -2,7 +2,9 @@ import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export default function CountButton({ type, setCount }) {
   const handleClick = () =>
-    setCount((prev) => (type === "plus" ? prev + 1 : prev - 1));
+    setCount((prev) =>
+      type === "plus" ? prev + 1 : prev - 1 < 0 ? 0 : prev - 1
+    );
 
   return (
     <button onClick={handleClick} className="count-btn">
