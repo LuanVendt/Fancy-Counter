d4c56c497d27b24849dd050ef1296da4aa1c7765
LuanVendt <luanvendt@gmail.com>
2025-01-18 17:15:05 -0300

    feat: create reusable button component

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,17 +1,21 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
-export default function CountButtons({ setCount }) {
+export default function CountButton({ type, setCount }) {
   const handlePlusButtonClick = () => setCount((prev) => prev + 1);
   const handleMinusButtonClick = () => setCount((prev) => prev - 1);
 
   return (
-    <div className="button-container">
-      <button onClick={handleMinusButtonClick} className="count-btn">
+    <button
+      onClick={
+        type === "minus" ? handleMinusButtonClick : handlePlusButtonClick
+      }
+      className="count-btn"
+    >
+      {type === "minus" ? (
         <MinusIcon className="count-btn-icon" />
-      </button>
-      <button onClick={handlePlusButtonClick} className="count-btn">
+      ) : (
         <PlusIcon className="count-btn-icon" />
-      </button>
-    </div>
+      )}
+    </button>
   );
 }
