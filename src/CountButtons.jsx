21c9ad17c7eaa6d89600a776a4866f8318e3ced7
LuanVendt <luanvendt@gmail.com>
2025-01-18 16:57:30 -0300

    refactor: updating buttons to use handler functions

--- src/CountButtons.jsx
+++ src/CountButtons.jsx
@@ -1,22 +1,15 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export default function CountButtons({ setCount }) {
+  const handlePlusButtonClick = () => setCount((prev) => prev + 1);
+  const handleMinusButtonClick = () => setCount((prev) => prev - 1);
+
   return (
     <div className="button-container">
-      <button
-        onClick={() => {
-          setCount((prev) => prev - 1);
-        }}
-        className="count-btn"
-      >
+      <button onClick={handleMinusButtonClick} className="count-btn">
         <MinusIcon className="count-btn-icon" />
       </button>
-      <button
-        onClick={() => {
-          setCount((prev) => prev + 1);
-        }}
-        className="count-btn"
-      >
+      <button onClick={handlePlusButtonClick} className="count-btn">
         <PlusIcon className="count-btn-icon" />
       </button>
     </div>
